9ca24f06e08965054c7b84e38372724caf0c4850
Chandra Shekar <mchandrashekar842@gmail.com>
2024-02-25 13:08:48 +0530

    config file fixed

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     database: {
       username: process.env.DB_USERNAME || 'root',
       password: process.env.DB_PASSWORD || 'Chandu@21',
-      database: process.env.DB_NAME || 'e_commerce',
+      database: process.env.DB_NAME || 'e-commerce',
     },
   };
   
